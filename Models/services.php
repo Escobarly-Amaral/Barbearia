5993fd88c80208e3af62511ee08dd9c1396fd7b0
Escobarly-Amaral <escobarlyamarall@gmail.com>
2024-07-28 22:40:47 -0300

    Model services atualizado.

---
+++ Models/services.php
@@ -0,0 +1,75 @@
+<?php
+    require_once('../Database/db.php');
+
+    class servicesModel{
+        use connectdb;
+
+        public function listServices(){
+            try{
+                $conn = $this->loadDB();
+                $conn = $this->conn;
+                $sql = $conn->prepare("SELECT * FROM services ORDER BY id");
+                $sql->execute();
+                $result = $sql->fetchAll();
+
+                return $result;
+            }catch(Exception $e){
+                echo "<pre>Erro: " . $e->getMessage() . "</pre>";
+            }
+        }
+
+        public function addService(){
+            try{
+                $conn = $this->loadDB();
+                $conn = $this->conn;
+
+                $maxid = $conn->prepare("SELECT id FROM services ORDER BY id DESC LIMIT 1");
+                $maxid->execute();
+                $lastid = $maxid->fetchColumn();
+                $id = ($lastid !== false) ? $lastid + 1 : 1;
+
+                $desc = NULL;
+                $name = NULL;
+                $price = NULL;
+
+                $sql = $conn->prepare("INSERT INTO services (id, name, description, price)
+                                        VALUES (:id, :name, :desc, :price)");
+                $sql->bindParam(':id', $id);
+                $sql->bindParam(':desc', $desc);
+                $sql->bindParam(':name', $name);
+                $sql->bindParam(':price', $price);
+                $sql->execute();
+                
+            }catch(Exception $e){
+                echo "<pre>Erro: " . $e->getMessage() . "</pre>";
+            }
+        }
+
+        public function setService(){
+            $conn = $this->loadDB();
+            $conn = $this->conn;
+
+            $sql = $conn->prepare("UPDATE services SET name=:name, description=:description, price=:price WHERE id = :id");
+
+            $id = $data['id'];
+            $description = $data['description'];
+            $name = $data['name'];
+            $price = $data['price'];
+
+            $sql->bindParam(':id', $id);
+            $sql->bindParam(':password', $password);
+            $sql->bindParam(':name', $name);
+            $sql->bindParam(':mail', $price);
+            $sql->execute();
+        }
+
+        public function removeUser($id){
+            $conn = $this->loadDB();
+            $conn = $this->conn;
+
+            $sql = $conn->prepare("DELETE FROM services WHERE id = :id");
+            $sql->bindParam(':id', $id);
+            $sql->execute();
+        }
+    }
+?>
